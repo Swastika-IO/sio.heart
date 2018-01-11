94d4c12d0a44afbfbde6b0867ddf50c26505bd1d
Huy Nguyen <i.love.to.smile.around@gmail.com>
2018-01-11 14:25:29 +0700

    Change package name to Swastika (without IO)

---
+++ src/Swastika/Domain/Core/Repository/DefaultRepositories.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Swastika.Domain.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Swastika.Domain.Data.Repository
+{
+    public class DefaultRepository<TContext, TModel>: ModelRepositoryBase<TContext, TModel>         
+        where TContext: DbContext
+        where TModel : class
+    {
+        private static volatile DefaultRepository<TContext, TModel> instance;
+        private static object syncRoot = new Object();
+
+        private DefaultRepository() { }
+
+        public static DefaultRepository<TContext, TModel> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new DefaultRepository<TContext, TModel>();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public class DefaultRepository<TDbContext, TModel, TView> : 
+        Swastika.Domain.Data.Repository.ViewRepositoryBase<TDbContext, TModel, TView>
+        where TDbContext : DbContext
+        where TModel : class
+        where TView : Swastika.Domain.Data.ViewModels.ViewModelBase<TDbContext, TModel, TView>
+    {
+        private static volatile DefaultRepository<TDbContext, TModel, TView> instance;
+        private static object syncRoot = new Object();
+
+        private DefaultRepository() { }
+
+        public static DefaultRepository<TDbContext, TModel, TView> Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new DefaultRepository<TDbContext, TModel, TView>();
+                    }
+                }
+
+                return instance;
+            }
+        }        
+    }
+}
---
+++ src/Swastika/UI/Core/Controllers/ApiHubController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Swastika.Api.Controllers;
+using Swastika.UI.Core.SignalR;
+
+namespace Swastika.UI.Core.Controllers
+{
+    public abstract class ApiHubController<THub, TDbContext, TModel>
+        : BaseApiController<TDbContext, TModel>
+        where TDbContext : DbContext
+        where TModel : class
+        where THub : BaseSignalRHub
+    {
+        private readonly IHubContext<THub> _hub;
+        public IHubClients Clients { get; private set; }
+        public IGroupManager Groups { get; private set; }
+        protected ApiHubController(IHubContext<THub> hub)
+        {
+            _hub = hub;
+            Clients = _hub.Clients;
+            Groups = _hub.Groups;
+        }
+    }
+
+    public abstract class ApiHubController<THub, TDbContext, TModel, TView> 
+        : BaseApiController<TDbContext, TModel, TView>
+        where TDbContext : DbContext
+        where TModel : class
+        where TView : Swastika.Domain.Data.ViewModels.ViewModelBase<TDbContext, TModel, TView>
+        where THub : BaseSignalRHub
+    {
+        private readonly IHubContext<THub> _hub;
+        public IHubClients Clients { get; private set; }
+        public IGroupManager Groups { get; private set; }
+        protected ApiHubController(IHubContext<THub> hub)
+        {
+            _hub = hub;
+            Clients = _hub.Clients;
+            Groups = _hub.Groups;
+        }
+    }
+}
